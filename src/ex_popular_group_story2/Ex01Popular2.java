af1caec23e2cb051c56e53081bc3c1db114a8bc5
Sakai <manae.sakai@rakus-partners.co.jp>
2024-01-24 17:47:47 +0900

    second commit

--- src/ex_popular_group_story2/Ex01Popular2.java
+++ src/ex_popular_group_story2/Ex01Popular2.java
@@ -11,46 +11,28 @@ public class Ex01Popular2 {
         String user ="postgres";
         String password = "postgres";
 
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        String sql = null;
-
-        try {
-            con = DriverManager.getConnection(url, user, password);
-            con.setAutoCommit(false);
-            sql = """
-                drop table if exists colors;
-                create table colors(
+        String sql = """
+                    drop table if exists colors;
+                    create table colors(
                     id integer primary key,
                     name text
-                );
-                """;
-            pstmt = con.prepareStatement(sql);
+                    );
+                    """;
+
+        try (Connection con = DriverManager.getConnection(url, user, password);
+            PreparedStatement pstmt = con.prepareStatement(sql)) {
+            
             int numOfUpdate = pstmt.executeUpdate();
             System.out.println(numOfUpdate + "件のデータを操作しました");
-            con.commit();
+            
         } catch (SQLException ex) {
             System.err.println("SQL=" + sql);
             ex.printStackTrace();
-            if (con != null) {
-                try {
-                    con.rollback();
-                } catch (SQLException er) {
-                    er.printStackTrace();
-                }
-            }
             
-        } finally {
-            try {
-                if (con != null) {
-                    con.close();
-                }
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch(SQLException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
+
+//con.setAutoCommit(false);
+//con.commit();
+//con.rollback();
--- src/ex_popular_group_story2/Ex01Popular2no2.java
+++ src/ex_popular_group_story2/Ex01Popular2no2.java
@@ -11,40 +11,25 @@ public class Ex01Popular2no2 {
         String user ="postgres";
         String password = "postgres";
 
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        String sql = null;
-
-        try {
-            con = DriverManager.getConnection(url, user, password);
-            sql = """
-                drop table if exists members;
-                create table members(
+        String sql = """
+                    drop table if exists members;
+                    create table members(
                     id serial primary key,
                     name text not null,
                     birth_day date,
                     gender varchar(1),
                     color_id integer references colors (id)
-                );
-                """;
-            pstmt = con.prepareStatement(sql);
+                    );
+                    """;
+        try (Connection con = DriverManager.getConnection(url, user, password);
+            PreparedStatement pstmt = con.prepareStatement(sql)) {
+
             int numOfUpdate = pstmt.executeUpdate();
             System.out.println(numOfUpdate + "件のデータを操作しました。");
 
         } catch (SQLException ex) {
             System.err.println("SQL=" + sql);
             ex.printStackTrace();
-        } finally {
-            try {
-                if (con != null) {
-                    con.close();
-                }
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch(SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        } 
     }
 }
